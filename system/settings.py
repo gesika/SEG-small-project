a51e2abbe1af6a25093bfac8fde7f9942128c5b5
Asif Alam <asif.alam@kcl.ac.uk>
2021-12-15 10:46:30 +0000

    Render messages in content pages

--- system/settings.py
+++ system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,8 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "clubs.User"
+
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'dark',
+    message_constants.DEBUG: 'danger',
+}
